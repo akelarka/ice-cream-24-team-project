cf2861fd59e021d22d2787ca82e7b5fcfb6f9cad
Oleksii Rozumii <o.rozumii@gmail.com>
2022-09-04 14:24:16 +0300

    добавил путь к ls

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-(() => {
-  const refs = {
-    openMenuBtn: document.querySelector('[data-menu-open]'),
-    closeMenuBtn: document.querySelector('[data-menu-close]'),
-    menu: document.querySelector('[data-menu]'),
-  };
-
-  refs.openMenuBtn.addEventListener('click', toggleModal);
-  refs.closeMenuBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.menu.classList.toggle('is-open');
-  }
-})();
+import './js/mobile-menu';
