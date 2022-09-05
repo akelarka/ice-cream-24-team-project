32d770e82cffd48bf5be78a5567961c29a3b3b88
Mariia Makarkina <mariiamak25@gmail.com>
2022-09-06 02:09:29 +0300

    Додає базовий слайд

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import './js/mobile-menu';
 import './js/prod';
+import $ from "jquery";
+import "slick-carousel";
+$(".carousel").slick({
+dots: true,
+infinite: true,
+slidesToShow: 1,
+slidesToScroll: 1,
+autoplay: true,
+autoplaySpeed: 2000,});
