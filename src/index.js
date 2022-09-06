f34a871c21c6ed8a080e39bec9ceae16560a4269
Mariia Makarkina <mariiamak25@gmail.com>
2022-09-06 04:13:07 +0300

    додає стиль для слайдів

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
 import './js/mobile-menu';
 import './js/prod';
-import $ from "jquery";
-import "slick-carousel";
-$(".carousel").slick({
-dots: true,
-infinite: true,
-slidesToShow: 1,
-slidesToScroll: 1,
-autoplay: true,
-autoplaySpeed: 2000,});
+import './js/slide';
