6ea982b69da5a2ac489e5b78cbf0e8e499e75115
D. Akelar <108392983+akelarka@users.noreply.github.com>
2022-09-06 23:30:25 +0300

    /

--- src/js/prod.js
+++ src/js/prod.js
@@ -9,22 +9,23 @@
     openProdBtn3: document.querySelector('[data-prod-open3]'),
     closeProdBtn3: document.querySelector('[data-prod-close3]'),
     prod3: document.querySelector('[data-prod3]'),
-  };
+    };
 
-  refs.openProdBtn.addEventListener('click', toggleProd);
-  refs.closeProdBtn.addEventListener('click', toggleProd);
-  refs.openProdBtn2.addEventListener('click', toggleProd2);
-  refs.closeProdBtn2.addEventListener('click', toggleProd2);
-  refs.openProdBtn3.addEventListener('click', toggleProd3);
-  refs.closeProdBtn3.addEventListener('click', toggleProd3);
+    refs.openProdBtn.addEventListener('click', toggleProd);
+    refs.closeProdBtn.addEventListener('click', toggleProd);
+    refs.openProdBtn2.addEventListener('click', toggleProd2);
+    refs.closeProdBtn2.addEventListener('click', toggleProd2);
+    refs.openProdBtn3.addEventListener('click', toggleProd3);
+    refs.closeProdBtn3.addEventListener('click', toggleProd3);
 
-  function toggleProd() {
-    refs.prod.classList.toggle('is-hidden__product');
-  }
-  function toggleProd2() {
-    refs.prod2.classList.toggle('is-hidden__product');
-  }
-  function toggleProd3() {
-    refs.prod3.classList.toggle('is-hidden__product');
-  }
+     function toggleProd() {
+     refs.prod.classList.toggle('is-hidden__product');
+    }
+     function toggleProd2() {
+     refs.prod2.classList.toggle('is-hidden__product');
+     }
+     function toggleProd3() {
+     refs.prod3.classList.toggle('is-hidden__product');
+    }
+              
 })();
