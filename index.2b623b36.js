a7d0385ea248b6debb05f40e85489f3e82ecae45
akelarka <108392983+akelarka@users.noreply.github.com>
2022-09-06 12:17:32 +0000

    Deploying to gh-pages from @ akelarka/ice-cream-24-team-project@033859499856d39d1aa50c85ec90ecd9a9ba6f52 🚀

---
+++ index.2b623b36.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-read-open]"),closeModalBtn:document.querySelector("[data-modal-read-close]"),modalwindow:document.querySelector("[data-modal-read-more]")};function o(){e.modalwindow.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.2b623b36.js.map
