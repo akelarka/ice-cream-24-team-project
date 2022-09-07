37f09aa886ae00f6e511bd8490a2a641ae4395a7
Anton-Ivanov-mkk <mister.kot.kyvalda.ivanov@gmail.com>
2022-09-07 15:08:59 +0300

    добавляет скрипт

--- src/js/modal.js
+++ src/js/modal.js
@@ -4,6 +4,8 @@
     closeModalBtn: document.querySelector("[data-modal-window-close]"),
     modalwindow: document.querySelector("[data-modal-window]"),
   };
+const openModalBtn = document.querySelectorAll("[data-modal-window-open]");
+  openModalBtn.forEach(element => element.addEventListener('click', toggleModal));
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
