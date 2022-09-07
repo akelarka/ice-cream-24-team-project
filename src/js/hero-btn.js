dc450ba426bee66dbe75e74034c5e9f7ca6a005b
D. Akelar <108392983+akelarka@users.noreply.github.com>
2022-09-07 12:05:15 +0300

    /

--- src/js/hero-btn.js
+++ src/js/hero-btn.js
@@ -5,12 +5,8 @@
     hero: document.querySelector('[data-hero]'),
     };
 
-    refs.openProdBtn.addEventListener('click', toggleProd);
-    refs.closeProdBtn.addEventListener('click', toggleProd);
-    refs.openProdBtn2.addEventListener('click', toggleProd2);
-    refs.closeProdBtn2.addEventListener('click', toggleProd2);
-    refs.openProdBtn3.addEventListener('click', toggleProd3);
-    refs.closeProdBtn3.addEventListener('click', toggleProd3);
+    refs.openProdBtn.addEventListener('click', toggleHero);
+    refs.closeProdBtn.addEventListener('click', toggleHero);
 
      function toggleHero() {
      refs.hero.classList.toggle('is-hidden__hero');
