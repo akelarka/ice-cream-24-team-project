e305afbecc802826765d33b0846625cd453ffdd5
D. Akelar <108392983+akelarka@users.noreply.github.com>
2022-09-07 11:54:21 +0300

    /

---
+++ src/js/hero-btn.js
@@ -0,0 +1,19 @@
+(() => {
+  const refs = {
+    openProdBtn: document.querySelector('[data-hero-open2]'),
+    closeProdBtn: document.querySelector('[data-hero-close2]'),
+    hero: document.querySelector('[data-hero]'),
+    };
+
+    refs.openProdBtn.addEventListener('click', toggleProd);
+    refs.closeProdBtn.addEventListener('click', toggleProd);
+    refs.openProdBtn2.addEventListener('click', toggleProd2);
+    refs.closeProdBtn2.addEventListener('click', toggleProd2);
+    refs.openProdBtn3.addEventListener('click', toggleProd3);
+    refs.closeProdBtn3.addEventListener('click', toggleProd3);
+
+     function toggleHero() {
+     refs.prod2.classList.toggle('is-hidden__hero');
+     }
+              
+})();
